e57103138974fe801b58ae42e88912fc19f002d5
pobredward <pobredward@gmail.com>
2025-03-26 16:15:56 +0900

    Fix Error "PageProp"

--- src/app/admin/job-board-manage/applicants/[id]/page.tsx
+++ src/app/admin/job-board-manage/applicants/[id]/page.tsx
@@ -6,12 +6,6 @@ export const metadata: Metadata = {
   description: '지원자 정보와 지원 현황을 관리할 수 있습니다.',
 };
 
-interface PageProps {
-  params: { id: string };
-  searchParams?: { [key: string]: string | string[] | undefined };
-}
-
-export default async function Page(props: PageProps) {
-  const { id } = props.params;
-  return <ApplicantsManageClient jobBoardId={id} />;
+export default function Page({ params }: { params: { id: string } }) {
+  return <ApplicantsManageClient jobBoardId={params.id} />;
 } 
