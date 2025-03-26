8b86c400f41112106ceaaf0f7fa1a80cdf2a3d44
pobredward <pobredward@gmail.com>
2025-03-26 16:04:40 +0900

    Fix Error "PageProp"

--- src/app/admin/job-board-manage/applicants/[id]/page.tsx
+++ src/app/admin/job-board-manage/applicants/[id]/page.tsx
@@ -1,18 +1,11 @@
 import { Metadata } from 'next';
-import { ApplicantsManageClient } from './ApplicantsManageClient';
+import { ApplicantsManageClient } from './client';
 
 export const metadata: Metadata = {
   title: '지원자 관리 - SMIS Mentor',
   description: '지원자 정보와 지원 현황을 관리할 수 있습니다.',
 };
 
-type Props = {
-  params: {
-    id: string;
-  };
-  searchParams: { [key: string]: string | string[] | undefined };
-};
-
-export default async function Page({ params }: Props) {
+export default function Page({ params }: { params: { id: string } }) {
   return <ApplicantsManageClient jobBoardId={params.id} />;
 } 
