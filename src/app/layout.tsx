d2ab3595fb204962c8fb23c4af25eae73eb6f2b3
pobredward <pobredward@gmail.com>
2025-04-01 13:57:23 +0900

    Edit "SEO"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,23 +9,23 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "SMIS 멘토 채용 플랫폼",
-  description: "에스엠아이에스(SMIS) 공식 채용 플랫폼입니다. 멘토 채용 및 교육 정보를 제공합니다.",
-  keywords: "SMIS, 에스엠아이에스, 멘토, 채용, 교육, 플랫폼, 임대환, 성남시, 분당구",
+  description: "SMIS 멘토 채용 플랫폼입니다.",
+  keywords: "SMIS, 영어캠프, 해외캠프, 국내캠프, 멘토, 채용, 교육",
   authors: [{ name: '에스엠아이에스' }],
   publisher: '에스엠아이에스',
   creator: '에스엠아이에스',
   metadataBase: new URL('https://www.smis-mentor.com'),
   verification: {
     // 구글 서치 콘솔에서 발급받은 인증 코드로 교체하세요
-    google: "구글_사이트_인증코드",
+    google: "vuF1TTsFjNoaKkVBHXKUbgNZRYcHi-S-SmAmn5kWIq0",
     other: {
       // 네이버 웹마스터 도구에서 발급받은 인증 코드로 교체하세요
-      "naver-site-verification": "네이버_사이트_인증코드",
+      "naver-site-verification": "3f732ac5d0f0d5ccd49580ade7c5b037189b6e86",
     },
   },
   openGraph: {
-    title: "SMIS - 에스엠아이에스 공식 채용 플랫폼",
-    description: "에스엠아이에스(SMIS) 공식 채용 플랫폼입니다. 멘토 채용 및 교육 정보를 제공합니다.",
+    title: "SMIS 멘토 채용 플랫폼",
+    description: "SMIS 멘토 채용 플랫폼입니다.",
     url: 'https://www.smis-mentor.com',
     siteName: 'SMIS 채용 플랫폼',
     images: [
@@ -63,7 +63,7 @@ export const metadata: Metadata = {
   },
   themeColor: '#ffffff',
   appleWebApp: {
-    title: 'SMIS 채용 플랫폼',
+    title: 'SMIS 멘토 채용 플랫폼',
     statusBarStyle: 'default',
   },
 };
@@ -76,7 +76,6 @@ export default function RootLayout({
   return (
     <html lang="ko" data-theme="light" className="light">
       <head>
-      <meta name="naver-site-verification" content="3f732ac5d0f0d5ccd49580ade7c5b037189b6e86" />
         <script dangerouslySetInnerHTML={{
           __html: `
             // 사용자의 테마 설정을 무시하고 항상 라이트 테마 적용
