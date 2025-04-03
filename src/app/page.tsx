04d3561663ae761fe2f3c980e15148f3e52f5172
pobredward <pobredward@gmail.com>
2025-04-03 17:19:37 +0900

    Edit UI "멘토 후기"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,9 +56,9 @@ async function ReviewsSection() {
                   </div>
                       <div>
                     <h3 className="font-bold text-lg text-gray-900 mb-1 line-clamp-2">{review.title}</h3>
-                    <div className="text-sm text-gray-500">
+                    {/* <div className="text-sm text-gray-500">
                       {review.createdAt && formatDate(review.createdAt)}
-                    </div>
+                    </div> */}
         </div>
       </div>
 
@@ -68,9 +68,9 @@ async function ReviewsSection() {
                   <svg className="h-4 w-4 text-yellow-400 mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
                   </svg>
-                  <span className="mr-2">{review.rating.toFixed(1)}</span>
-                  <span>|</span>
-                  <span className="ml-2">{review.writer}</span>
+                  {/* <span className="mr-2">{review.rating.toFixed(1)}</span> */}
+                  {/* <span>|</span> */}
+                  {/* <span className="ml-2">{review.writer}</span> */}
                 </div>
                 </Link>
               ))}
