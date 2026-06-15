f934bc76781cabec1e47293386a3ef586051dbc8
pobredward <pobredward@gmail.com>
2026-06-16 06:48:44 +0900

    Edit Version "1.6.6"

--- packages/mobile/app.config.ts
+++ packages/mobile/app.config.ts
@@ -72,7 +72,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     ...config,
     name: 'SMIS Mentor',
     slug: 'smis-mentor',
-    version: '1.6.5',
+    version: '1.6.6',
     orientation: 'portrait',
     icon: './assets/icon.png',
     userInterfaceStyle: 'light',
